fb509a91acd990d62a35e9591a04c8b66c579ced
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2022-08-15 17:24:50 +0200

    213445

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
 const sectionHeroEl = document.querySelector(".section-hero");
+
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
     console.log(ent);
+
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
     }
+
     if (ent.isIntersecting === true) {
       document.body.classList.remove("sticky");
     }
   },
   {
-    //in the viewport
+    // In the viewport
     root: null,
-    thershold: 0,
+    threshold: 0,
     rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroEl);
+
 //////////////////////////////////////////////////////
 //set current year
 //////////////////////////////////////////////////////
